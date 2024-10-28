a233765199c2fda83a4733262d4de8cc9d37e143
amyx-x <amy.liddington@student.norwichuni.ac.uk>
2024-10-28 10:11:52 +0000

    completed in class

--- js/simple-player.js
+++ js/simple-player.js
@@ -1,4 +1,62 @@
 
+// Define constants for interactive html elements
+const playPauseButton = document.getElementById("play-pause-button")
+const currentTimeH4 = document.getElementById("current-time")
+const totalTime =document.getElementById("total-time")
+const seekBar = document.getElementById("seek-bar")
+// audio object that stores audio file, permits playback etc...
+const myAudio = new Audio("audio/Soft-Background-for-Interview.webm")
+// boolean to track seek state
+let isSeeking = false
+// core playback functions
+console.log(playPauseButton)
+playPauseButton.onclick = function(){
+// myAudio.paused == true (shorthand v)
+    if(myAudio.paused){
+        myAudio.play()
+    }else{
+        myAudio.pause()
+    }
+}
+// when audio plays, change icon to pause
+myAudio.onplay= function(){
+    playPauseButton.src="images/pause.svg"
+}
+// when audio pauses, change icon to play
+myAudio.onpause= function(){
+    playPauseButton.src="images/play.svg"
+}
+// once we have track info available, set the total time and seek bar
+myAudio.onloadedmetadata = function(){
+    totalTime.innerHTML = formatTime(myAudio.duration)
+    // round the duration down to the nearest whole number for the slider to work
+    seekBar.max = Math.floor(audio.duration)
+}
+// update current time with audio playback 
+myAudio.ontimeupdate = function(){
+    currentTimeH4.innerHTML = formatTime(myAudio. currentTime)
+    // update seek bar when not seeking
+    if(!isSeeking){ // same as isSeeking == false
+        // round down current time for the slider
+        seekBar.value =Math.floor(myAudio.currentTime)
+    }
+}
+// on seekbar user imput set isSeeking to true
+seekBar.oninput =function(){
+    isSeeking =true
+}
+// on seekbar change e.g. dragged by user 
+seekBar.onchange =function(){
+    // update the audio current time to reflect seek bar poisition 
+    myAudio.currentTime =seekBar.value
+    // reset seekig state
+    isSeeking = false 
+}
+// when finished, reset 
+myAudio.onended = function(){
+    currentTimeH4.innerHTML=formatTime(0)
+    seekBar.value=0;
+}
 
 /**
  * This formatTime function will format time from seconds to a human readable time.
